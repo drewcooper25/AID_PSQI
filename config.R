8d2ea98caf9cd70674d24b294b8e9843ee35f06b
drewcooper25 <drdrew.cooper@gmail.com>
2026-09-07 18:34:32 +0200

    config.R: generate empty folders for first-time script runners

--- config.R
+++ config.R
@@ -6,3 +6,20 @@ setwd(script_dir)
 
 # Define the data directory; this MUST be in the same parent folder as AID_PSQI for anything to work
 data_dir <- normalizePath(file.path("..", "OPEN-Project-data"))
+
+# Generate the empty folders for first time script users
+create_dirs <- function(dirs) {
+  for (dir in dirs) {
+    if (!dir.exists(dir)) {
+      dir.create(dir, recursive = TRUE)
+    }
+  }
+}
+
+output_dirs <- c(
+  "archive",
+  "figures",
+  "output"
+)
+
+create_dirs(output_dirs)
